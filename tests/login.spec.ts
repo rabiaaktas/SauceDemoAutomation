e0ab8d82fba21fd41bced095337b507416eb8fe7
rabiaaktas <aktasrabiaa@gmail.com>
2024-10-07 23:17:29 +0300

    new scen added

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,6 +8,13 @@ test.describe("Login Tests", () => {
     await page.locator('#user-name').fill('standard_user')
     await page.locator('#password').fill('secret_sauce')
     await page.click('[id="login-button"]')
+    await expect(page.locator("[data-test='title']")).toHaveText("Products")
+  });
+  test("check error message for locked user", async ({ page }) => {
+    await page.locator('#user-name').fill('locked_out_user')
+    await page.locator('#password').fill('secret_sauce')
+    await page.click('[id="login-button"]')
+    await expect(page.locator("[data-test='error']")).toHaveText("Epic sadface: Sorry, this user has been locked out.")
   });
   
 })
